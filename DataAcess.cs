72e98cf00b61ceeccf683d71fe2d8d074eec9a9a
Stefan Risteski <38658609+Berkampe@users.noreply.github.com>
2018-06-14 09:49:42 +0200

    Add files via upload
    Smeneti Os i Dvizenje

---
+++ DataAcess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Organizacija_na_farma
+{
+    public class DataAcess
+    {
+
+        public SqlConnection conn { get; set; }
+
+        public SqlConnection getConnection()
+        {
+            conn= new SqlConnection("Data Source=DESKTOP-2QPPOB4;Initial Catalog=FARMA;Integrated Security=True");
+            return conn;
+        }
+
+        public void cmdCommand(SqlCommand cmd)
+        {
+            SqlDataReader reader;
+            try
+            {
+                conn.Open();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+
+                }
+                reader.Close();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString()); 
+            }
+        }
+
+   }
+}
---
+++ OSForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Organizacija_na_farma
+{
+    partial class OSForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonIzbrisi = new System.Windows.Forms.Button();
+            this.buttonDodadi = new System.Windows.Forms.Button();
+            this.buttonPromeni = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonIzbrisi
+            // 
+            this.buttonIzbrisi.Location = new System.Drawing.Point(167, 58);
+            this.buttonIzbrisi.Name = "buttonIzbrisi";
+            this.buttonIzbrisi.Size = new System.Drawing.Size(129, 62);
+            this.buttonIzbrisi.TabIndex = 0;
+            this.buttonIzbrisi.Text = "Избриши";
+            this.buttonIzbrisi.UseVisualStyleBackColor = true;
+            this.buttonIzbrisi.Click += new System.EventHandler(this.buttonIzbrisi_Click);
+            // 
+            // buttonDodadi
+            // 
+            this.buttonDodadi.Location = new System.Drawing.Point(12, 58);
+            this.buttonDodadi.Name = "buttonDodadi";
+            this.buttonDodadi.Size = new System.Drawing.Size(129, 62);
+            this.buttonDodadi.TabIndex = 1;
+            this.buttonDodadi.Text = "Додади";
+            this.buttonDodadi.UseVisualStyleBackColor = true;
+            this.buttonDodadi.Click += new System.EventHandler(this.buttonDodadi_Click);
+            // 
+            // buttonPromeni
+            // 
+            this.buttonPromeni.Location = new System.Drawing.Point(326, 58);
+            this.buttonPromeni.Name = "buttonPromeni";
+            this.buttonPromeni.Size = new System.Drawing.Size(129, 62);
+            this.buttonPromeni.TabIndex = 2;
+            this.buttonPromeni.Text = "Промени податоци";
+            this.buttonPromeni.UseVisualStyleBackColor = true;
+            this.buttonPromeni.Click += new System.EventHandler(this.buttonPromeni_Click);
+            // 
+            // OSForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 178);
+            this.Controls.Add(this.buttonPromeni);
+            this.Controls.Add(this.buttonDodadi);
+            this.Controls.Add(this.buttonIzbrisi);
+            this.Name = "OSForm";
+            this.Text = "Единка";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonIzbrisi;
+        private System.Windows.Forms.Button buttonDodadi;
+        private System.Windows.Forms.Button buttonPromeni;
+    }
+}
